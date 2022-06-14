a8b9255ea4973643540a72a01dad7ac7690c79da
milangjdev <107418822+milangjdev@users.noreply.github.com>
2022-06-15 01:17:20 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+-- LOGOVI
+Config.Webhook = ''
+Config.Ime = 'Admin Meni Logovi'
+Config.Slika = ''
+
+-- NOTIFIKACIJE
+Config.MythicNotify = true
+Config.okokNotify = false
+Config.DefaultNotify = false
